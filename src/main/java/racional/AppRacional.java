6550047da576cc682c468cb078390a47846de977
alopezp90 <alopezp90@gmail.com>
2020-12-10 13:16:37 +0100

    Crea main()

---
+++ src/main/java/racional/AppRacional.java
@@ -0,0 +1,19 @@
+package racional;
+
+/**
+ * @author Alberto López Puertas
+ * <alopezp90@gmail.com>
+ */
+public class AppRacional {
+
+    public static void main(String[] args) {
+        Racional z = new Racional();
+        z.setNumerador(4);
+        z.setDenominador(5);
+
+        Racional k = new Racional();
+        k.setNumerador(3);
+        k.setDenominador(4);
+
+    }
+}
