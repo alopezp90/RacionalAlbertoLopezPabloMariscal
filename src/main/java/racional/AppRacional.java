95d49e4e4e1f1d389c735f8acb33b70e8ef19d57
alopezp90 <alopezp90@gmail.com>
2020-12-10 13:37:49 +0100

    Divide z y k y comenta el codigo

--- src/main/java/racional/AppRacional.java
+++ src/main/java/racional/AppRacional.java
@@ -7,23 +7,34 @@ package racional;
 public class AppRacional {
 
     public static void main(String[] args) {
+        //Crea z
         Racional z = new Racional();
         z.setNumerador(4);
         z.setDenominador(5);
 
+        //Crea k
         Racional k = new Racional();
         k.setNumerador(3);
         k.setDenominador(4);
 
+        //Crea racional aleatorio
         Racional aleatorio = Racional.aleatorio();
         
+        //Imprime los valores de los racionales, redondeados a 2 decimales
         System.out.printf("z = %.2f \n", (double)z.getNumerador() / z.getDenominador());
         System.out.printf("k = %.2f \n", (double)k.getNumerador() / k.getDenominador());
         System.out.printf("aleatorio = %.2f \n", (double)aleatorio.getNumerador() / aleatorio.getDenominador());
         
+        //Compara z y k
         System.out.println("¿Son iguales z y k? " + Racional.igualdad(z,k));
         
+        //Suma z y k, lo guarda en z y lo imprime
         System.out.println("Sumamos z y k: ");
         (z.suma(k)).imprimirConsola();
+        
+        
+        //Guarda en un nuevo objeto la division z/k y lo imprime
+        Racional x = z.division(k);
+        x.imprimirConsola();
     }
 }
